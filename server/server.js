bb7e215dd242b2f72ddc3b7d4c1adcbb85d5cbc8
sushaz9 <susannahelisabeth1@gmail.com>
2023-11-03 13:51:13 +0000

    init bruv

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+const axios = require("axios");
+
+app.get("/", (request, response) => {
+  response.json("You are looking at root route");
+
+  app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+});
